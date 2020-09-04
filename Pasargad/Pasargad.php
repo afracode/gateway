16039aef1c922cb416b4fe9429c2978863b5e48f
Ali Sharifi Neyestani <sh4rifi@gmail.com>
2020-09-04 23:01:23 +0430

    Added transactions methods

--- Pasargad/Pasargad.php
+++ Pasargad/Pasargad.php
@@ -4,6 +4,8 @@
 namespace App\Helpers\Pasargad;
 
 
+use App\Models\Payment;
+
 class Pasargad
 {
     public $terminalCode = '***';
@@ -39,6 +41,60 @@ class Pasargad
 
         return \View::make('callback.pasargad-redirector')->with(compact('url', 'redirectUrl', 'invoiceNumber', 'invoiceDate', 'amount', 'terminalCode', 'merchantCode', 'timeStamp', 'action', 'sign'));
     }
+
+
+    public function newTransaction($price, $invoiceId)
+    {
+        $this->transactionId = Payment::insertGetId([
+            'port' => 'PASARGAD',
+            'price' => $price,
+            'status' => 'INIT',
+            'ip' => \Request::ip(),
+            'user_id' => Auth::id() ?? null,
+            'invoice_id' => $invoiceId,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+
+        return $this->transactionId;
+    }
+
+
+    protected function transactionFailed($resultObj)
+    {
+        Payment::whereId($this->transactionId)->update([
+            'status' => 'FAILED',
+            'tracking_code' => $resultObj['transactionReferenceID'],
+            'updated_at' => Carbon::now(),
+        ]);
+
+    }
+
+
+    protected function transactionSucceed($resultObj)
+    {
+        Payment::whereId($this->transactionId)->update([
+            'status' => 'SUCCEED',
+            'tracking_code' => $resultObj['transactionReferenceID'],
+            'ref_id' => $resultObj['referenceNumber'],
+            'card_number' => $resultObj['cardNumber'] ?? null,
+            'payment_date' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+    }
+
+
+    protected function newLog($statusCode, $statusMessage)
+    {
+        return DB::table('gateway_transactions_logs')->insert([
+            'transaction_id' => $this->transactionId,
+            'result_code' => $statusCode,
+            'result_message' => $statusMessage,
+            'log_date' => Carbon::now(),
+        ]);
+    }
+
+
 
 
 
